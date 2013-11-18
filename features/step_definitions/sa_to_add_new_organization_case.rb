d6c457b7ddee5a53e7d73ef9f64e90fdf70aae92
Bapu Labade <bapu.labade@aretove.com>
2013-11-18 20:33:05 +0530

    changes for Create organization feature

--- features/step_definitions/sa_to_add_new_organization_case.rb
+++ features/step_definitions/sa_to_add_new_organization_case.rb
@@ -12,26 +12,24 @@ admin.update_attributes(fk_role_id: role.id)
 end
 
 Given(/^I click on create organization$/) do
-	binding.pry
-	click_link "Create Organization"
+  click_link "Create Organization"
 end
 
 Then(/^I should see a form$/) do
   page.should have_content("Create Organization")
-  page.should have_button("Create Organization")
 end
 
 When(/^I fill in form with proper organization details and submit$/) do
-  fill_in "organization_name", Faker::Company.name
-  fill_in "organization_address1", Faker::Address.street_address
-  fill_in "organization_address2", Faker::Address.street_address
-  fill_in "organization_contact_first_name", Faker::Name.first_name
-  fill_in "organization_contact_last_name", Faker::Name.last_name
-  fill_in "organization_contact_email", Faker::Internet.email
+  fill_in "organization_name", with: Faker::Company.name
+  fill_in "organization_address1", with: Faker::Address.street_address
+  fill_in "organization_address2", with: Faker::Address.street_address
+  fill_in "organization_contact_first_name", with: Faker::Name.first_name
+  fill_in "organization_contact_last_name", with: Faker::Name.last_name
+  fill_in "organization_contact_email",  with: Faker::Internet.email
   click_button "Create Organization"
 end
 
 Then(/^I should see success message and organization details$/) do
   page.should have_content("Organization was created successfully.")
-  page.should have_content("Organization Details.")
+  page.should have_content(/Organization Details./)
 end
