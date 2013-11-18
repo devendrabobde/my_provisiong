31f76142f76b94a9ed40cd568e65f1bc324f0238
Bapu Labade <bapul@sparkway.com>
2013-11-18 19:58:33 +0530

    SA Test Case

--- onestop-provisioning/features/step_definitions/sa_to_add_new_organization_case.rb
+++ onestop-provisioning/features/step_definitions/sa_to_add_new_organization_case.rb
@@ -3,6 +3,7 @@ admin = Cao.create(email: Faker::Internet.email, username: Faker::Internet.user_
 first_name: Faker::Name.first_name , last_name: Faker::Name.last_name,
 password: "password", password_confirmation: "password")
 role = Role.create(name: "Admin")
+Organization.delete_all
 # organization = Organization.create(name: Faker::Company.name, address1: Faker::Address.street_address, 
 # address2: Faker::Address.street_address, contact_first_name: Faker::Name.first_name, 
 # contact_last_name: Faker::Name.last_name, contact_email: Faker::Internet.email)
@@ -10,6 +11,27 @@ admin.update_attributes(fk_role_id: role.id)
 @current_cao = admin
 end
 
-# Given(/^I click on create organization$/) do
+Given(/^I click on create organization$/) do
+	binding.pry
+	click_link "Create Organization"
+end
 
-# end
+Then(/^I should see a form$/) do
+  page.should have_content("Create Organization")
+  page.should have_button("Create Organization")
+end
+
+When(/^I fill in form with proper organization details and submit$/) do
+  fill_in "organization_name", Faker::Company.name
+  fill_in "organization_address1", Faker::Address.street_address
+  fill_in "organization_address2", Faker::Address.street_address
+  fill_in "organization_contact_first_name", Faker::Name.first_name
+  fill_in "organization_contact_last_name", Faker::Name.last_name
+  fill_in "organization_contact_email", Faker::Internet.email
+  click_button "Create Organization"
+end
+
+Then(/^I should see success message and organization details$/) do
+  page.should have_content("Organization was created successfully.")
+  page.should have_content("Organization Details.")
+end
