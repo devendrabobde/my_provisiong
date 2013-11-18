a2988a80932d78393424e024e65aed21fb69300e
Bapu Labade <bapul@sparkway.com>
2013-11-18 19:16:02 +0530

    SA Test Case

--- features/step_definitions/sa_to_add_new_organization_case.rb
+++ features/step_definitions/sa_to_add_new_organization_case.rb
@@ -6,6 +6,10 @@ role = Role.create(name: "Admin")
 # organization = Organization.create(name: Faker::Company.name, address1: Faker::Address.street_address, 
 # address2: Faker::Address.street_address, contact_first_name: Faker::Name.first_name, 
 # contact_last_name: Faker::Name.last_name, contact_email: Faker::Internet.email)
-coa.update_attributes(fk_role_id: role.id)
+admin.update_attributes(fk_role_id: role.id)
 @current_cao = admin
 end
+
+# Given(/^I click on create organization$/) do
+
+# end
