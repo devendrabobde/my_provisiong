7593ac343508ca8d42e3f15d1d863db445a2c036
Bapu Labade <bapul@sparkway.com>
2013-11-22 18:44:02 +0530

    Code Updates

--- app/controllers/admin/caos_controller.rb
+++ app/controllers/admin/caos_controller.rb
@@ -2,16 +2,19 @@ class Admin::CaosController < ApplicationController
 
   before_filter :find_value, only: [:new, :create, :edit, :update]
 
+  # Eager load organizations with CAOs
   def index
     @organization = Organization.where("SYS_ORGANIZATION_ID = ?", params[:organization_id]).includes(:caos => [:role, :profile]).first
     #@caos =  @organization.caos
   end
 
+  # Form for new CAO
   def new
     @organization = Organization.find(params[:organization_id])
     @cao = Cao.new
   end
 
+  # Create a CAO. Next step after new.
   def create
     @organization = Organization.find(params[:organization_id])
     @cao = @organization.caos.create(params[:cao])
@@ -24,16 +27,19 @@ class Admin::CaosController < ApplicationController
     end
   end
 
+  # get Details of a CAO
   def show
     @organization = Organization.find(params[:organization_id])
     @cao = Cao.find(params[:id])
   end
 
+  # Edit a particular CAO
   def edit
     @organization = Organization.find(params[:organization_id])
     @cao = Cao.find(params[:id])
   end
 
+  # Update a CAO with changes
   def update
     @organization = Organization.find(params[:organization_id])
     @cao = Cao.find(params[:id])
@@ -48,6 +54,7 @@ class Admin::CaosController < ApplicationController
     end
   end
 
+  # Delete a CAO (Basically, deactivate the CAO)
   def destroy
     @organization = Organization.find(params[:organization_id])
     @cao = Cao.find(params[:id])
@@ -55,6 +62,7 @@ class Admin::CaosController < ApplicationController
     redirect_to admin_organization_caos_path(@organization.id), :notice => "COA " + @cao.username + " deactivated successfully."
   end
 
+  # Revert an inactive CAO to active state. Thus, the CAO would be functional
   def activate
     @organization = Organization.find(params[:organization_id])
     @cao = Cao.find(params[:id])
@@ -63,6 +71,7 @@ class Admin::CaosController < ApplicationController
   end
 
   private
+  # Value pre-population
   def find_value
     @profile_list = Profile.all
     @role_list = Role.where(name: "COA")
