ae15aabfee1e6c62202bd470f311f4b6307c87ad
Bapu Labade <bapu.labade@aretove.com>
2013-12-03 17:22:51 +0530

    changes while generating factory data

--- onestop-provisioning/spec/factories/provider_dea_numbers.rb
+++ onestop-provisioning/spec/factories/provider_dea_numbers.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :provider_dea_number do
-    provider_dea "MyString"
-    provider_dea_state "MyString"
-    provider_dea_expiration_date "MyString"
-    fk_provider_app_detail_id "MyString"
+    provider_dea "FM1859860"
+    provider_dea_state "MD"
+    provider_dea_expiration_date "03/01/1988"
+    fk_provider_app_detail_id 1
   end
 end
