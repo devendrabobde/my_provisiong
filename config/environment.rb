4f8f03e408b1b953788b9820d14b459f03d3627b
Abhishek <abhisheks@sparkway.com>
2013-12-10 16:58:18 +0530

    Added a code

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 OneStop::Application.initialize!
 # ActiveRecord::Base.connection.raw_connection.exec("alter session set nls_numeric_characters = '.,'")
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # We're in smart spawning mode.
+    if forked
+      # Re-establish redis connection
+      require 'redis'
+
+      $redis.client.disconnect unless $redis.nil?
+      $redis = Redis.new(:host => 'localhost', :port => 6379)
+    end
+  end
+end
