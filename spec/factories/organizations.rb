d03ee71acdbc236dbb301bafea805f5fafa2024e
Bapu Labade <bapu.labade@aretove.com>
2013-12-25 12:38:45 +0530

    change in test data

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -5,7 +5,8 @@ FactoryGirl.define do
     address2 Faker::Address.street_address
     city Faker::Address.city
     state_code Faker::Address.state
-    postal_code Faker::Address.zip_code
+    postal_code "12345"
+    zip_code "12345"
     country_code "+52"
     contact_first_name Faker::Name.first_name
     contact_last_name Faker::Name.last_name
