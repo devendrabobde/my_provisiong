9a7b71ee538fd6ee127c07a4d1223447d963f9a0
Bapu Labade <bapu.labade@aretove.com>
2013-12-27 14:41:06 +0530

    added rspec test cases for SuperNPI application is unreachable (network interruption) at the time when an upload begins and Router application is unreachable (network interruption) at the time when an upload begins

---
+++ onestop-provisioning/spec/lib/onestop_router_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../spec_helper'
+
+describe "OneStopRouter" do
+  context "class methods" do
+	  describe '#batch_upload' do
+	    describe "Router application is unreachable (network interruption) at the time when an upload begins" do
+	    	it "raises an timeout error when onestop router batch_upload method is called" do
+	    		application = RegisteredApp.where(app_name: "EPCS-IDP").first
+	    		providers =  [{ :npi=>"1114919727", :last_name=>"KISTNER", :first_name=>"LISA" }, 
+	    		              { :npi=>"1114000148", :last_name=>"MOULICK", :first_name=>"ACHINTYA"}] 
+	    		response = OnestopRouter::batch_upload(providers, application)
+	    		response[:error].should =~ /Connection refused/
+	    	end
+	    end
+	  end
+	end
+end
