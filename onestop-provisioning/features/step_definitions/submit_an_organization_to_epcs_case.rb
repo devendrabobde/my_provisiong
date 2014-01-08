c627bea56aaa752fa6758e574d168b61b77b8f2f
Aditya Narsapurkar <aditya.narsapurkar@aretove.com>
2014-01-08 17:00:56 +0530

    Submit Org To EPCS Test Case

--- onestop-provisioning/features/step_definitions/submit_an_organization_to_epcs_case.rb
+++ onestop-provisioning/features/step_definitions/submit_an_organization_to_epcs_case.rb
@@ -3,5 +3,23 @@ When /^I select a csv file with single provider passing WsBatchIdp$/ do
 end
 
 When /^I select a csv file with single provider failing WsBatchIdp$/ do
+	@app_validation = RegisteredApp.where(app_name: "EPCS-IDP").first.app_upload_fields.where(name: 'state').first
+	@app_validation.update_attribute(:required, false)
 	attach_file 'upload', File.join(Rails.root, 'public', 'rspec_test_files', 'epcs', 'single_provider_with_all_required_fields.csv')
+end
+
+When /^I select a csv file with single provider passing WsBatchIdp and with already registered Organization$/ do
+	attach_file 'upload', File.join(Rails.root, 'public', 'rspec_test_files', 'epcs', 'single_provider_passing_batchidp.csv')
+end
+
+And /^I should be able to see the error message from WsBatchIdp$/ do
+	@app_validation.update_attribute(:required, true)
+	page.all(:css, "#table1 tbody tr").each do |td|
+    	td.text.split(" ").last.should_not =~ /Success/
+  	end
+end
+
+
+When /^When I select csv file having single provider failing WsBatchIdp and with registered Organization$/ do
+	attach_file 'upload', File.join(Rails.root, 'public', 'rspec_test_files', 'epcs', 'single_provider_failing_batchidp.csv')
 end
