d7e6f002d53e4c4a47f48a540f3a3ade38c157c9
Aditya Narsapurkar <aditya.narsapurkar@aretove.com>
2014-01-08 19:57:30 +0530

    Test case Updated

--- onestop-provisioning/features/step_definitions/submit_an_organization_to_epcs_case.rb
+++ onestop-provisioning/features/step_definitions/submit_an_organization_to_epcs_case.rb
@@ -3,7 +3,7 @@ When /^I select a csv file with single provider passing WsBatchIdp$/ do
 end
 
 When /^I select a csv file with single provider failing WsBatchIdp$/ do
-	
+	attach_file 'upload', File.join(Rails.root, 'public', 'rspec_test_files', 'epcs', 'single_provider_failing_batchidp.csv')
 end
 
 When /^I select a csv file with single provider passing WsBatchIdp and with already registered Organization$/ do
