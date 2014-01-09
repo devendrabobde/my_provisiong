ea3a60ad66290899f9fe852fa2d3e286db634a5d
Aditya Narsapurkar <aditya.narsapurkar@aretove.com>
2014-01-09 17:33:39 +0530

    Changes in tests

--- onestop-provisioning/features/step_definitions/ui_validation_ok_epcs_api_wssetuporganizationstatus_fails.rb
+++ onestop-provisioning/features/step_definitions/ui_validation_ok_epcs_api_wssetuporganizationstatus_fails.rb
@@ -3,12 +3,12 @@ Given /^valid COA credentials$/ do
     first_name: Faker::Name.first_name , last_name: Faker::Name.last_name,
     password: "password", password_confirmation: "password")
   role = Role.create(name: "COA")
-  @organization_coa = Organization.create(name: Faker::Company.name, address1: Faker::Address.street_address,
+  @organization_coa_valid = Organization.create(name: Faker::Company.name, address1: Faker::Address.street_address,
     address2: Faker::Address.street_address, contact_first_name: Faker::Name.first_name,
     contact_last_name: Faker::Name.last_name, contact_email: Faker::Internet.email, zip_code: "12345", 
     postal_code: "54321", state_code: "AL123")
-  coa.update_attributes(fk_role_id: role.id, fk_organization_id: @organization_coa.id)
-  @current_cao = coa
+  coa.update_attributes(fk_role_id: role.id, fk_organization_id: @organization_coa_valid.id)
+  @current_valid_cao = coa
 end
 
 When /^I select a csv file which contains single batch of multiple providers$/ do
@@ -19,4 +19,14 @@ And /^I should be able to see failure message for failed WsSetUpOrganizationStat
   page.all(:css, "#table1 tbody tr").each do |td|
     td.text.split(" ").last.should_not =~ /Success/
   end
+end
+
+And /^I fill in the username and password with valid credentials$/ do
+  fill_in "cao_username", with: @current_valid_cao.username
+  fill_in "cao_password", with: "password"
+end
+
+
+And /^I should be able to associate provider with COA with valid credentials$/ do
+  ProviderAppDetail.where(fk_cao_id: @current_valid_cao.id).count > 0
 end
