938505fcb3769417fc1e9f48511f1156347a7589
Bapu Labade <bapul@sparkway.com>
2014-01-30 12:15:38 +0530

    MOXY-11882: Implement performance logging - added server config yml file

--- onestop-provisioning/config/initializers/constants.rb
+++ onestop-provisioning/config/initializers/constants.rb
@@ -5,3 +5,4 @@ US_STATES = ["AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FM", "
 	"MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", 
 	"MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT",
 	"VT", "VI", "VA", "WA", "WV", "WI", "WY"]
+SERVER_CONFIGURATION = YAML.load(File.open("#{Rails.root}/config/server.yml"))
