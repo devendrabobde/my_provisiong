648d17560d41c9e7463641290be521f676941a43
Bapu Labade <bapul@sparkway.com>
2014-02-04 15:06:53 +0530

    MOXY:11891- OneStop Provisioning System should save the performance log in the database.

--- app/middlewares/performance_log_middleware.rb
+++ app/middlewares/performance_log_middleware.rb
@@ -4,7 +4,6 @@ class PerformanceLogMiddleware
   end
 
   def call(env)
-
     puts "--------------------------------------------------"
     @env     = env
     @request = Rack::Request.new(@env)
@@ -13,7 +12,7 @@ class PerformanceLogMiddleware
       start_time = Time.now
       user_agent = UserAgent.parse(@request.user_agent)
       performance_log = PerformanceLog.new
-      performance_log.request_time    = Time.now
+      performance_log.request_time    = start_time
       performance_log.request_ip      = @request.ip
       performance_log.request_content = "#{@request.request_method} #{@request.path}"
       performance_log.client_platform = user_agent.browser
@@ -26,24 +25,28 @@ class PerformanceLogMiddleware
         path = File.join(directory, name)
         File.open(path, "w") { |f| f.write(upload[:tempfile].read.gsub(/[\"\'\-\!\$\%\^\&\*\(\)\+\=\{\}\;\`\?\|\<\>\]\[]/, "")) }
       end
-      performance_log.request_params  = @request.params.to_json[0..1998]
-      performance_log.server_name     = SERVER_CONFIGURATION["production"]["service_name"]
-      performance_log.server_version  = SERVER_CONFIGURATION["production"]['code_version']
+      performance_log.request_params        = @request.params.to_json[0..1998]
+      performance_log.server_name           = SERVER_CONFIGURATION["onestop_service_name"]
+      performance_log.server_version        = SERVER_CONFIGURATION['onestop_code_version']
+      performance_log.server_ip             = SERVER_CONFIGURATION['onestop_server_ip']
+      performance_log.server_instance_name  = SERVER_CONFIGURATION['onestop_server_instance_name']
 
       call_rails
-      rails_route_params = env['action_dispatch.request.parameters']
 
+      rails_route_params = env['action_dispatch.request.parameters']
       response_body = ""
       @response.each { |part| response_body += part }
-
-      performance_log.client_id        = env['client_id']
-      performance_log.response_time    = (((Time.now - start_time).seconds) * 1000).to_i
-      performance_log.response_content = response_body
-      performance_log.status            = @headers['ResponseStatus']
-      performance_log.error_code        = @headers['ErrorCode']
-      performance_log.error_description = @headers['ErrorMessage']
-      performance_log.controller_name   = rails_route_params['controller']
-      performance_log.action_name       = rails_route_params['action']
+      performance_log.client_id           = @request.ip
+      response_time                       = (((Time.now - start_time).seconds) * 1000).to_i
+      performance_log.response_time       = response_time
+      performance_log.response_content    = response_body.to_s.truncate(2000) rescue nil
+      performance_log.error_code          = @response.status rescue nil
+      performance_log.error_description   = @response.status_message rescue nil
+      db_response_time                    = @response.request.env['action_controller.instance'].db_runtime rescue nil
+      performance_log.db_response_time    = db_response_time
+      performance_log.total_response_time = response_time + db_response_time.to_i
+      performance_log.controller_name     = rails_route_params['controller']
+      performance_log.action_name         = rails_route_params['action']
       performance_log.save
     else
       call_rails
