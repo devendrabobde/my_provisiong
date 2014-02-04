c4f3acdda4f3c3af2ae202f3bfe5efae328abd72
Bapu Labade <bapul@sparkway.com>
2014-02-04 15:06:53 +0530

    MOXY:11891- OneStop Provisioning System should save the performance log in the database.

--- onestop-provisioning/app/models/performance_log.rb
+++ onestop-provisioning/app/models/performance_log.rb
@@ -1,8 +1,9 @@
 class PerformanceLog < ActiveRecord::Base
   
-  attr_accessible :controller_name, :server_name, :server_version, :server_ip, :action_name,
-   :client_id, :request_params, :request_ip, :request_content, :request_time, 
-   :response_content, :client_platform, :client_version, :status, :error_code, :error_description, :response_time
+  attr_accessible :controller_name, :server_name, :server_version, :server_ip, :server_instance_name, 
+      :action_name, :client_id, :request_params, :request_ip, :request_content, :request_time, 
+      :response_content, :response_time, :client_platform, :client_version, :error_code, :error_description,
+      :db_response_time, :total_response_time
    
   include Extensions::UUID
 
