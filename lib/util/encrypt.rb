90a0a68941cf00ca5489f4be7867544d61fead21
Aditya Narsapurkar <adityan@sparkway.com>
2014-02-05 14:11:17 +0530

    Added Encryption Liabrary

---
+++ lib/util/encrypt.rb
@@ -0,0 +1,17 @@
+require 'base64'
+	module Util
+		class Encrypt
+		def self.encrypt(value)
+			Base64.encode64(Encryptor.encrypt(value,:key => self.get_key))
+		end
+
+		def self.decrypt(value)
+			Encryptor.decrypt(Base64.decode64(value),:key => get_key)
+		end
+
+		private
+		def self.get_key
+			Base64.decode64(File.read("#{Rails.root}/config/.dbpass"))
+		end
+	end
+end
