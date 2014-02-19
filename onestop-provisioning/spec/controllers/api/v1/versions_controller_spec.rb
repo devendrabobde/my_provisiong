6881f9a2deddd77bf7dcb21a06930594a1974b53
Bapu Labade <bapul@sparkway.com>
2014-02-19 12:30:12 +0530

    MOXY-12112 - Update to check version consistent with ESI application

--- onestop-provisioning/spec/controllers/api/v1/versions_controller_spec.rb
+++ onestop-provisioning/spec/controllers/api/v1/versions_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Api::V1::VersionsController do
   describe "GET 'check_version'" do
-    it "returns http success" do
+    it "As a User, I should be able to verify application up and runining status and DB status and it should return http success" do
       get 'check_version'
       response.should be_success
     end
