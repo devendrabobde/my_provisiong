82c7e65f876eec012fb63a842d5e1db0be05fc64
abhishek <abhisheks@sparkway.com>
2014-02-24 17:58:41 +0530

    MOXY-12276 Moxy OIS Cucumber/Rspec Test cases for Moxy valid and invalid scenarios

---
+++ onestop-provisioning/features/step_definitions/moxy_upload_providers_valid_case.rb
@@ -0,0 +1,7 @@
+Given /^I select moxy application$/ do
+  select "Moxy", from: 'provider_registered_app_id'
+end
+
+When /^I select a csv file of valid moxy providers$/ do
+  attach_file 'upload', File.join(Rails.root, 'public', 'rspec_test_files', 'moxy', 'valid_moxy_providers.csv') # id
+end
