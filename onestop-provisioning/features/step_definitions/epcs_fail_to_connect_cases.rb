f5966baf809ae6dd2d70a2874e3adeeede38b550
Aditya Narsapurkar <adityan@sparkway.com>
2014-03-11 14:01:59 +0530

    MOXY 12596 updates

--- onestop-provisioning/features/step_definitions/epcs_fail_to_connect_cases.rb
+++ onestop-provisioning/features/step_definitions/epcs_fail_to_connect_cases.rb
@@ -3,9 +3,13 @@ And /^I should be able to see (.+?) acknowledgement or error messages$/ do |app_
 		page.all(:css, "#table1 tbody tr").each do |td|
     		td.text.should =~ /Connection Error/
   		end
+	elsif app_name == "Rcopia"
+		page.all(:css, "#table1 tbody tr").each do |td|
+	    	td.text.should have_content("No route to host")
+	  	end
 	else
 		page.all(:css, "#table1 tbody tr").each do |td|
-	    td.text.should have_content("Connection refused")
-	  end
+	    	td.text.should have_content("Connection refused")
+	  	end
 	end
 end
