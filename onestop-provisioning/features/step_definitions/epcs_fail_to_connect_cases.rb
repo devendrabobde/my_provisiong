63b176577b08013adf017475569db883c519296e
Aditya Narsapurkar <adityan@sparkway.com>
2014-03-11 13:54:44 +0530

    Moxy 12596 Rcopia fail to connect case and test step normalization

--- onestop-provisioning/features/step_definitions/epcs_fail_to_connect_cases.rb
+++ onestop-provisioning/features/step_definitions/epcs_fail_to_connect_cases.rb
@@ -1,5 +1,11 @@
-And /^I should be able to see EPCS acknowledgement or error messages$/ do
-  page.all(:css, "#table1 tbody tr").each do |td|
-    td.text.should =~ /EPCS-IDP OIS: EPCS-IDP Connection refused/
-  end
+And /^I should be able to see (.+?) acknowledgement or error messages$/ do |app_name|
+	if app_name == "onestop-router"
+		page.all(:css, "#table1 tbody tr").each do |td|
+    		td.text.should =~ /Connection Error/
+  		end
+	else
+		page.all(:css, "#table1 tbody tr").each do |td|
+	    td.text.should have_content("Connection refused")
+	  end
+	end
 end
