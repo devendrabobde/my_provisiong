6d0d89a74a56445befcfeeefe142a11114d84ffb
Aditya Narsapurkar <adityan@sparkway.com>
2014-03-11 15:20:17 +0530

    12596 Rcopia ois fail to connect cases

--- features/step_definitions/epcs_fail_to_connect_cases.rb
+++ features/step_definitions/epcs_fail_to_connect_cases.rb
@@ -3,10 +3,6 @@ And /^I should be able to see (.+?) acknowledgement or error messages$/ do |app_
 		page.all(:css, "#table1 tbody tr").each do |td|
     		td.text.should =~ /Connection Error/
   		end
-	elsif app_name == "Rcopia"
-		page.all(:css, "#table1 tbody tr").each do |td|
-	    	td.text.should have_content("No route to host")
-	  	end
 	else
 		page.all(:css, "#table1 tbody tr").each do |td|
 	    	td.text.should have_content("Connection refused")
