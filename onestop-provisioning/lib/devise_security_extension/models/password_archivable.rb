d10fa49b134e16eb4da65808e0d2d3c25a52dd4f
Aditya Narsapurkar <adityan@sparkway.com>
2014-04-08 18:08:20 +0530

    Password Ois Updates

--- onestop-provisioning/lib/devise_security_extension/models/password_archivable.rb
+++ onestop-provisioning/lib/devise_security_extension/models/password_archivable.rb
@@ -1,6 +1,6 @@
 class OldPassword < ActiveRecord::Base
-  require "#{Rails.root}/app/models/extensions/uuid.rb"
-  include Extensions::UUID
+  # require "#{Rails.root}/app/models/extensions/uuid.rb" 
+  # include Extensions::UUID
 
   attr_accessible :encrypted_password, :password_salt
   
