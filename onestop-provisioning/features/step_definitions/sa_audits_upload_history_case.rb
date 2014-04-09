8b70deaaa832671a907bd7f1034d2e3256dcf1e3
Aditya Narsapurkar <adityan@sparkway.com>
2014-04-09 21:08:12 +0530

    Test case updates

--- onestop-provisioning/features/step_definitions/sa_audits_upload_history_case.rb
+++ onestop-provisioning/features/step_definitions/sa_audits_upload_history_case.rb
@@ -1,7 +1,7 @@
 And (/^I should be successfully logged out of the application$/) do
   sleep 5
   visit current_path
-	click_on(@current_cao.user_name)
+	click_on(@current_cao.username)
 	click_on('Logout')
 	page.should have_content("LOG IN")
 end
