d3a43155841d8e3be8345ac486f7dcb240a2a7d8
Bapu Labade <bapul@sparkway.com>
2014-04-16 13:32:58 +0530

    MOXY-13256 - Improvement of Onestop-Provisioning RSpec test case coverage

---
+++ onestop-provisioning/spec/workers/redis_update_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "RedisUpdate" do
+  describe "#perform" do
+  	it "should perform cache validation update" do
+  	  response = RedisUpdate.perform
+  	  assert response.should be_nil
+  	end
+  end
+end
