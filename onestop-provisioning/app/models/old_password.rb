8e2336e913beaf8c54c5f668d6152340962c320b
Bapu Labade <bapul@sparkway.com>
2014-04-25 11:01:17 +0530

    MOXY-13313 - COA is not asked to reset their password when they log in the first time

--- onestop-provisioning/app/models/old_password.rb
+++ onestop-provisioning/app/models/old_password.rb
@@ -2,7 +2,7 @@ class OldPassword < ActiveRecord::Base
 
   include Extensions::UUID
   
-  attr_accessible :encrypted_password, :password_salt
+  attr_accessible :encrypted_password, :password_salt, :password_archivable_type, :password_archivable_id
 
   belongs_to :password_archivable, :polymorphic => true
 
