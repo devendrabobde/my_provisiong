56c0a4528ab8fe9837b8357d5b8cafeeca022ac5
Bapu Labade <bapul@sparkway.com>
2014-04-25 14:19:09 +0530

    MOXY-13313 - COA is not asked to reset their password when they log in the first time

--- onestop-provisioning/features/step_definitions/upload_npi_providers_already_registered_case.rb
+++ onestop-provisioning/features/step_definitions/upload_npi_providers_already_registered_case.rb
@@ -7,5 +7,6 @@ Given /^a valid COA having NPI providers already registered with the COAs organi
     address2: Faker::Address.street_address, contact_first_name: Faker::Name.first_name,
     contact_last_name: Faker::Name.last_name, contact_email: Faker::Internet.email, zip_code: "12345")
   coa.update_attributes(fk_role_id: role.id, fk_organization_id: @organization_coa.id)
+  coa.old_passwords.create(encrypted_password: "Password@1234", password_archivable_type: "Cao", password_archivable_id: coa.id)
   @current_cao = coa
 end
