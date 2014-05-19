ea344f7c2189e02a3264778a76f729ce98d40d6a
Bapu Labade <bapul@sparkway.com>
2014-05-19 11:07:05 +0530

    test case code update

--- spec/lib/rcopia_ois_spec.rb
+++ spec/lib/rcopia_ois_spec.rb
@@ -33,56 +33,7 @@ describe "RCOPIA-OIS" do
 		                        [""=>[{:provider_dea=>"BV8234661"}]],
 		                      "email" => "test@example.com"
 		                    }]
-		                    @router_reg_apps =  [
-    {
-      "DrFirst" =>  [
-          {
-              "authentication_url" =>  "https://applicationtwo_sparkway.home => 3007/users/login",
-              "enrollment_url" =>  "http://10.100.10.211/users/sign_up",
-              # "ip_address_concat" =>  "10.100.10.45",
-              "ip_address_concat" => CONSTANT['EPCS_OIS']['TEST_SERVER_URL'],
-              "validate_csv_url" => "http://localhost:3003/api/v1/ois/validations/validate-csv.json",
-              "validate_provider_url" => "http://localhost:3003/api/v1/ois/validations/validate-provider.json",
-              'batch_upload_url' => "http://10.100.10.45/api/v1/ois/epcs/batch_upload_dest.json",
-              "ois_name" =>  "epcsidp",
-              "ois_password" =>  "sImI0WW9oxfM00wLjX0TQuOxM3QiwItCesVPkf8NkTB8HqMuPpBGVURgGlKNM9mWZ"
-          },
-          {
-              "authentication_url" =>  "http://moxy/users/login",
-              "enrollment_url" =>  "http://moxy/users/sign_up",
-              "validate_csv_url" => "http://localhost:3004/api/v1/ois/validations/validate-csv.json",
-              "validate_provider_url" => "http://localhost:3004/api/v1/ois/validations/validate-provider.json",
-              'batch_upload_url' => 'http://10.100.10.45:81/api/v1/ois/moxy/batch_upload_dest.json',
-              # "ip_address_concat" =>  "10.100.10.45",
-              "ip_address_concat" => CONSTANT['MOXY_OIS']['TEST_SERVER_URL'],
-              "ois_name" =>  "moxy",
-              "ois_password" =>  "Nr7aICD51pBigc9pac3LBjYobvavptxfF0yNON6nktOwjP8ZmQT7UxxR8wxWUQQ6H5Il"
-          },
-          {
-              "authentication_url" =>  "http://rcapia/users/login",
-              "enrollment_url" =>  "http://rcopia/users/sign_up",
-              # "ip_address_concat" =>  "10.100.10.45",
-              "ip_address_concat" => CONSTANT['RCOPIA_OIS']['TEST_SERVER_URL'],
-              "validate_csv_url" => "http://localhost:3002/api/v1/ois/validations/validate-csv.json",
-              "validate_provider_url" => "http://localhost:3002/api/v1/ois/validations/validate-provider.json",
-              'batch_upload_url' => "http://10.100.10.45:84/api/v1/ois/rcopia/batch_upload_dest.json",
-              "ois_name" =>  "rcopia",
-              "ois_password" =>  "vCr8eTgeYE6grgjePkNA135iG6OU0W36z8py1jSM5bzco6dWfamaMlPjl2iiXogI7xc"
-          }
-      ]
-  },
-  {
-      "Org1" =>  [
-          {
-              "authentication_url" =>  "http://test.com/login",
-              "enrollment_url" =>  "http://test.com/sign_up",
-              "ip_address_concat" =>  "test.com",
-              "ois_name" =>  "OIS1",
-              "ois_password" =>  "FA7wjlaRVzPX3SvfiYjXd2UvMSa7tFpkRPQLdlTCJxNJwxctRO1XtryNrr"
-          }
-      ]
-  }
-]
+		                    @router_reg_apps = OnestopRouter.request_batchupload_responders(nil)
 			    	end
 			    	it "After uploading a valid NPI, Rcopia Ois should have the expected result" do		    			
 			    		response = ProvisioningOis::batch_upload_dest(@providers, @coa, @application, @router_reg_apps).last
