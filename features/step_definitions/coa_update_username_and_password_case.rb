980bb1b3df9ef5cebc59fe9c878712d49b94d812
Bapu Labade <bapul@sparkway.com>
2014-05-28 22:01:41 +0530

    MOXY-13930 - COA Edit Account screen has Bad design

--- features/step_definitions/coa_update_username_and_password_case.rb
+++ features/step_definitions/coa_update_username_and_password_case.rb
@@ -1,6 +1,7 @@
-Given(/^I click on edit account link in setting section$/) do
+And(/^I click on edit account link in setting section$/) do
   @current_cao.old_passwords.create(encrypted_password: "Password@1234", password_archivable_type: "Cao", password_archivable_id: @current_cao.id)
-  visit edit_cao_registration_path
+  click_link("Setting")
+  click_link("Edit Personal Info")
   @current_cao.old_passwords.delete_all
 end
 
@@ -8,13 +9,25 @@ Then(/^I should be able to see edit account form$/) do
   page.should have_content("Personal Information")
 end
 
+When(/^I click on change password link in setting section$/)do
+  @current_cao.old_passwords.create(encrypted_password: "Password@1234", password_archivable_type: "Cao", password_archivable_id: @current_cao.id)
+  page.execute_script("$('#update-password-modal').modal('hide');")
+  click_link("Setting")
+  click_link("Change Password")
+  @current_cao.old_passwords.delete_all
+end
+
+Then (/^I should be able to see change password form$/) do
+  page.should have_content("Change Password")
+end
+
 And(/^I click on update with the information that needs to be changed including correct old password$/) do
   fill_in('cao_password', :with => 'password@12345')
   fill_in('cao_password_confirmation', :with => 'password@12345')
   fill_in('cao_current_password', :with => 'password@123')
-  within(".change-password-form") do
-    click_on("Update")
-  end  
+  # within(".change-password-form") do
+    click_button("Update")
+  # end  
 end
 
 Then(/^I should see success message You updated your account successfully$/) do
@@ -25,7 +38,7 @@ And(/^I click on update with the information that needs to be changed including 
   fill_in('cao_first_name', :with => 'Sarah')
   fill_in('cao_last_name', :with => 'Parkar')
   fill_in('cao_email', :with => 'sarahparkar@onestop.com')
-  within(".personal-info-form") do
-    click_on("Update")
-  end
+  # within(".personal-info-form") do
+    click_button("Update")
+  # end
 end
