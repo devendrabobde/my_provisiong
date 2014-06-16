fbe05cc42dbae33949efca05146397c2d0550c71
Bapu Labade <bapul@sparkway.com>
2014-06-16 15:53:27 +0530

    Test case Code update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:cao]
   end
+
+  ### Overriding rails NavbarHelper methods for adding custom css class
+  
+  def drop_down_link(name)
+    link_to(name_and_caret(name), "#", :class => "dropdown-toggle username-dropdown", "data-toggle" => "dropdown")
+  end
+
 end
