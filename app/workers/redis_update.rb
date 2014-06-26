b8297a34be2364d83331c3ec43cf6388b6a512d8
Bapu Labade <bapul@sparkway.com>
2014-06-26 17:28:30 +0530

    Redis cache code update

--- app/workers/redis_update.rb
+++ app/workers/redis_update.rb
@@ -1,6 +1,6 @@
 # Cache the different types of application validations as per batch upload models
 class RedisUpdate
-  # @queue = :cached_updation
+  @queue = :cached_updation
   # def self.perform
   #   RedisCache.update_validations_cached
   # end
